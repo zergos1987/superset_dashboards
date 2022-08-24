a944e09033df94fb6a41291fb3b96c3427fd7b59
Mischenko Vasily <85062042+zergos1987@users.noreply.github.com>
2022-08-24 09:52:30 +0300

    Delete Superset_Redis_Service.bat

--- root/Superset_Redis_Service.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set current_service_path=%~dp0
-cmd.exe /K "%current_service_path%\Redis-x64-3.0.504\redis-server.exe --dbfilename dump.rdb --dir %current_service_path%\Redis-x64-3.0.504"
